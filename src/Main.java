10e59480d1f63d773a3ca0a5f688803b195f08d2
aphidrs <aphidrs@gmail.com>
2023-01-22 01:47:14 +0100

    Menu & console messages added

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,18 @@ class Main {
         boolean continuar = true;
 
         while (continuar) {
-            System.out.println("\nMenú:");
+            System.out.println("");
+            System.out.println("     ======");
+            System.out.println("    | Menú |");
+            System.out.println("     ======");
+            System.out.println("");
             System.out.println("1. Añadir nuevo coche");
             System.out.println("2. Borrar coche por id");
             System.out.println("3. Consultar coche por id");
             System.out.println("4. Listado de coches");
             System.out.println("5. Terminar el programa");
             System.out.println("6. Exportar coches a archivo CSV");
+            System.out.println("");
             System.out.print("Seleccione una opción: ");
             int opcion = scanner.nextInt();
             scanner.nextLine();
@@ -38,6 +43,7 @@ class Main {
                     String color = scanner.nextLine();
                     Coche coche = new Coche(id, matricula, marca, modelo, color);
                     almacen.addCoche(coche);
+                    System.out.println("[Info] Coche: " + coche + " se ha añadido correctamente.");
                     break;
             
                 case 2: 
@@ -51,9 +57,9 @@ class Main {
                     scanner.nextLine();
                     if (almacen.idExists(id, almacen.coches)) {
                         almacen.deleteCoche(id);
-                        System.out.println("Coche borrado con éxito.");
+                        System.out.println("[Info] El coche con ID " + id + " se ha borrado correctamente.");
                     } else {
-                        System.out.println("El ID introducido no existe");
+                        System.out.println("[Warning] El ID introducido no existe");
                     }
                     break;
                 case 3: 
@@ -67,9 +73,11 @@ class Main {
                     scanner.nextLine();
                     Coche cocheConsultado = almacen.getCoche(id);
                     if (cocheConsultado != null) {
-                        System.out.println("ID: " + cocheConsultado.getId() + " - Matrícula: " + cocheConsultado.getMatricula() + " - Marca: " + cocheConsultado.getMarca() + " - Modelo: " + cocheConsultado.getModelo() + " - Color: " + cocheConsultado.getColor());
+                        System.out.println("");
+                        System.out.println("[Info] Consulta ID: " + cocheConsultado.getId() + " - Matrícula: " + cocheConsultado.getMatricula() + " - Marca: " + cocheConsultado.getMarca() + " - Modelo: " + cocheConsultado.getModelo() + " - Color: " + cocheConsultado.getColor());
+                        System.out.println("");
                     } else {
-                        System.out.println("No existe un coche con ese ID.");
+                        System.out.println("[Warning] No existe un coche con ese ID.");
                     }
                     break;
                 case 4: almacen.listCoches();break;
@@ -79,9 +87,11 @@ class Main {
                     break;
                 case 6:
                     almacen.exportarCochesCSV();
-                    System.out.println("Coches exportados con éxito.");
+                    System.out.println("");
+                    System.out.println("[Info] Coches exportados con éxito.");
+                    System.out.println("");
                     break;
-                default: System.out.println("Opción no válida, por favor seleccione una opción del menú.");break;
+                default: System.out.println("[Warning] Opción no válida, por favor seleccione una opción del menú.");break;
             }
         }
         scanner.close();
